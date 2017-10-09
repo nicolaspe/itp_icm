bcb35e7fdbe6e7bec37a7e709fda55b2d9ede631
Nicolas Peña Escarpentier <npescarpentier@gmail.com>
2017-10-09 12:10:19 -0400

    pointlight trials html files added

--- class04/lightworld/lightTrials.js
+++ class04/lightworld/lightTrials.js
@@ -6,9 +6,9 @@ function setup(){
   //move your mouse to change light direction
   var dirX = (mouseX / width - 0.5) *2;
   var dirY = (mouseY / height - 0.5) *(-2);
-  directionalLight(250, 0, 0, dirX, dirY, 1);
+  // directionalLight(250, 0, 0, dirX, dirY, 1);
 	var zAxis = map(mouseX, 0, width, -PI, PI);
-	// pointLight(0, 0, 250, dirX, dirY, -0.6);
+	pointLight(0, 0, 250, dirX, dirY, -0.6);
 
   ambientMaterial(250);
   sphere(100);
